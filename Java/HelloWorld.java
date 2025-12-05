6504176ac6eb73567f1ee12b5e211e4910864cd2
Anele Nonkululeko Ndlovu <an3l3ndlovu@gmail.com>
2025-12-05 14:57:18 +0200

    Add HelloWorld class with introduction message

---
+++ Java/HelloWorld.java
@@ -0,0 +1,5 @@
+public class Main { 
+public static void main (String [] args ) { 
+  System.out.println("Hello, my name is Anele and this is my introduction to Java. This isn't my first attempt.Pray for me. "); 
+} 
+} 
